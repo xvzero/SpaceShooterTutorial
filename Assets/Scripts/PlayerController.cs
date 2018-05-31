41c4a229dd5ae76f4cd8391074c9bee935de4891
Andre <25297095+xvzero@users.noreply.github.com>
2018-05-31 16:43:05 -0700

    Setup background, camera, lighting, player object, and player controls. Add shooting functionality

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour 
+{
+
+    public Boundary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float speed, tilt, fireRate;
+
+    private float nextFire;
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rigidbody.velocity = movement * speed;
+
+        rigidbody.position = new Vector3
+        (
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+            0f,
+            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rigidbody.rotation = Quaternion.Euler(0f, 0f, rigidbody.velocity.x * -tilt);
+	}
+}
